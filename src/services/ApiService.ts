14009fef83286866872a15df10a60ad56ec95064
zobla <zobla.kv@gmail.com>
2025-02-26 22:55:41 +0100

    remove comment

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,11 +1,8 @@
-// TODO: create models and update types
-
 export const get = async (uuid: string): Promise<any> => {
   return fetch('http://localhost:3000/entities/' + uuid);
 }
 
 export const add = async (data: any): Promise<any> => {
-  console.log('post')
   return fetch('http://localhost:3000/entities', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
